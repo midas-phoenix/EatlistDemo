11bb5e53129a82c81fb2240561e2ee4fc4bda7b1
jhaytaslim <jhaytaslim@gmail.com>
2018-03-21 12:17:22 +0100

    search_roll

---
+++ EatlistApi/Controllers/UtilsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EatListDataService.DataBase;
+using EatListDataService.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EatlistApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Utils")]
+    public class UtilsController : Controller
+    {
+        public ILogger<dynamic> _log;
+        public readonly UtilRepository _utilRepo = new UtilRepository();
+        private static UserManager<ApplicationUser> _userManager;//= new UserManager<ApplicationUser>();
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        // GET: api/<controller>
+        /// <summary>
+        /// searches the db for text
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet, Route("search/{word}")]
+        public async Task<ActionResult> GetViewable(string word)
+        {
+            try
+            {
+                ApplicationUser userId = await GetCurrentUserAsync();
+                return Ok(_utilRepo.Search(word));
+            }
+            catch (Exception ex)
+            {
+                _log.LogInformation(ex.Message + ex.StackTrace);
+                return StatusCode(500);
+            }
+
+        }
+
+    }
+}
